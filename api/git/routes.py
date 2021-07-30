1f62b89b7dfa87eb52f15c5b14eb62f7d14e9e4a
DralrinResthal <c.neilclack@gmail.com>
2021-07-30 12:32:26 -0500

    CON-2 add placeholders for each route

--- api/git/routes.py
+++ api/git/routes.py
@@ -9,3 +9,29 @@ bp = Blueprint("git", __name__, url_prefix="/git")
 @authorized
 def test():
     return "Git blueprint loaded.", 200
+
+
+@bp.route("/clone", methods=["GET"])
+def clone():
+    pass
+
+
+@bp.route("/branch", methods=["POST"])
+def branch():
+    pass
+
+
+@bp.route("/commit", methods=["POST"])
+def commit():
+    pass
+
+
+@bp.route("/push", methods=["POST"])
+def push():
+    pass
+
+
+# Because there is only 1 "github" route, it is in git
+@bp.route("/pr", methods=["POST"])
+def pr():
+    pass
